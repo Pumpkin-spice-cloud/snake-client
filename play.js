102c39456b2bca00c38b74a76e8597af9518b2ec
Pumpkin-spice-cloud <alan_mo@live.com>
2019-12-04 20:16:54 +0000

    added ability to leave

--- play.js
+++ play.js
@@ -1,3 +1,26 @@
-const  connecting  = require('./client');
+const connecting = require('./client');
 console.log('Connecting ...');
 connecting.connect();
+
+function handleUserInput(data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+}
+
+const setupInput = function () {
+
+  const stdin = process.stdin;
+  stdin.on('data', data =>{
+    handleUserInput(data);
+  });
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+
+
+  return stdin;
+}
+setupInput();
+
